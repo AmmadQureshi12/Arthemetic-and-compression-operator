3c321ff8d5bfcf60c8f07b34958f2bdeffe00c3a
Ammad Qureshi <159720868+AmmadQureshi12@users.noreply.github.com>
2024-04-28 15:00:49 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,143 @@
+// //Addition 
+// let ammad=1
+// let ammad2=2
+// console.log(ammad+ammad2);
+
+// //subtraction
+// let q1=2
+// let q2=1
+// console.log(q1-q2)
+
+// //Multiplication 
+// let q3=10
+// let q4=5
+// console.log(q3*q4)
+
+// //Divided
+// let q5=30
+// let q6=3
+// console.log(q5/q6)
+
+// //modulo
+// let q7=13001
+// let q8=30
+// console.log(q7%q8)
+
+// //comperision operator
+// let num1=10
+// let num2=20
+// let num3=5
+// let num4=20
+// //grater then
+// console.log(num1 > num2)
+// //           10  > 20  //true
+// console.log(num2 >= num4)
+// //           20  >= 20 //true
+// console.log(num1 >= num3)
+// //          10   >= 5 // true
+// console.log(num1 > num4)
+// //          10   > 20 //false
+// console.log(num1 > num4)
+// //          10   > 20 //false
+// console.log(num1 < num2)
+// //          10   < 20  // true
+// console.log(num1 <= num2)
+// //          10   <= 20 //true
+// console.log(num1 < num2)
+// //           10  < 20 //true
+// console.log(num1 >= num2)
+// //           10 >= 20 //false
+// console.log(num2 >= num4)
+// //            20 >= 20 //true
+// console.log(num2 >= num3)
+// //           20  >= 5 //true
+// console.log(num1 < num3)
+// //           20  < 5 //false
+// console.log(num2 === num4)
+//           20  === 20
+// console.log(num1 < num2)
+// //           20  < 10 //false
+// console.log(num1 != num2)
+// //           20  != 10  //false
+// console.log(num1 < num2)
+// //          20   < 10  //false
+// let a=10
+// let b=10
+// console.log(a == b)// == equal to sign
+// console.log(a != b)// != it is a not equal to sign.
+// console.log(a > b) // false
+// console.log(a < b)//true
+// console.log(a >= b)//false
+// console.log(a <= b)//true
+
+//  console.log(5 !== 5)//false
+// // console.log(5 !== "5")//true
+// //console.log("ammad" != "ammad1")//true
+// //console.log("car" !== "car" )//false
+// //console.log("5" !== 5)//false
+
+// let car ="subaru"
+// console.log(car != "city")//true
+// console.log(car !== "subaru")//false
+// console.log(car != "ammad")//true
+// console.log(car !== "1")//true
+// console.log(car !== "carolla")//true
+
+// console.log(1 + 1)//2 addition sign
+// console.log(1 - 1)//0 subtraction sign
+// console.log(2 * 3)//6 multiplication sign
+// console.log(10 / 2)//5 divided sign
+// console.log(24 % 5)//4 modules sign
+
+// //simple function
+// function anyname(){
+//     let name="ammad qureshi";
+//     console.log(name)
+// }
+// anyname()
+
+// //other function
+// function any_name(num1:number=5, num2:number=1){
+//     let y= num1+num2
+//     console.log(y)
+// }
+// any_name()
+
+// //function
+// function ammad(num1:number=10, num2:number=10){
+//     const ammr=num1*num2
+//     console.log(ammr)
+// }
+// ammad()
+
+// function anyname(name:string="Ammad Qureshi", age:number=22, fathername:string="kamyab qureshi"){
+//     console.log(`My Name is ${name}`)
+//     console.log(`My age is ${age}`)
+//     console.log(`My father Name is ${fathername}`)
+// }
+// anyname()
+
+// //return function
+// function sum(num1:number,num2:number){
+//     let y= num1+num2
+//     return y
+// }
+// console.log(sum(5,10));
+//return function
+function sum1(num3:number,num4:number){
+    let t= num3-num4
+    return t;
+}
+console.log(sum1(5,10))
+//return function
+function ammar(num1:number,num2:number){
+    let ya=num1/num2
+    return ya
+}
+console.log(ammar(10,2))
+//Arrow function
+let amma=(num1:number,num2:number)=>{
+    let ya=num1/num2
+    return ya
+}
+console.log(ammar(10,4))
